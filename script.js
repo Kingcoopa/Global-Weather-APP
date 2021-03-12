fd130cfe69587138866ffb175b7f1423a4332369
Kingcoopa <69878838+Kingcoopa@users.noreply.github.com>
2021-03-11 19:43:42 -0500

    Development of the search and clear

---
+++ script.js
@@ -0,0 +1,39 @@
+//Variable to contain searched locations
+var city="";
+
+// Declared Variables
+var searchLocation = $("#searchLocation");
+var searchClick = $("#searchButton");
+var clearSearches = $("#clearAll");
+var searchedLocations=[];
+
+// Search to see if the location exists in previous entries in local storage
+function find(c){
+    for (var i=0; i<searchedLocations.length; i++){
+        if(c.toUpperCase()===searchedLocations[i]){
+            return -1;
+        }
+    }
+    return 1;
+}
+
+// API key
+var APIKey="3f5eeceaae5ba710debb32e410bc047d";
+
+// Show curent and future forecast after location search
+function showWeather(event){
+    event.preventDefault();
+    if(searchLocation.val().trim()!==""){
+        city=searchLocation.val().trim();
+        currentWeather(city);
+    }
+}
+
+//Clear History
+function clear(event){
+    event.preventDefault();
+    searchedLocations=[];
+    localStorage.removeItem("cityname");
+    document.location.reload();
+
+}
