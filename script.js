e48980f239f93647d77b837b569e68e89d1f88f4
Kingcoopa <69878838+Kingcoopa@users.noreply.github.com>
2021-03-13 20:10:43 -0500

    HTML Weather Card Built Ajax call and server pull

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ var city="";
 var searchLocation = $("#searchLocation");
 var searchClick = $("#searchButton");
 var clearSearches = $("#clearAll");
+var currentLocation = $("#currentLoc");
+var currentTemp = $("#temp");
+var currentHum= $("#hum");
+var currentWindSpeed=$("#windSpeed");
+var currentUvindex= $("#uvIndx");
 var searchedLocations=[];
 
 // Search to see if the location exists in previous entries in local storage
@@ -25,9 +30,21 @@ function showWeather(event){
     event.preventDefault();
     if(searchLocation.val().trim()!==""){
         city=searchLocation.val().trim();
-        currentWeather(city);
+        myWeather(city);
     }
 }
+
+//AJAX call
+function myWeather(city){
+    
+    // URL to pull server data
+    var openWeaURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
+    $.ajax({
+        url:openWeaURL,
+        method:"GET",
+    }).then(function(response){
+    
+
 
 //Clear History
 function clear(event){
